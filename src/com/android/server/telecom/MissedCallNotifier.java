CMRemix/android_packages_services_Telecomm

[4/4] Telecomm: Breathing missedcall/sms/voicemail
Base: https://gerrit.omnirom.org/#/c/4976/
Mms: https://gerrit.omnirom.org/#/c/4977/
OmniGears: https://gerrit.omnirom.org/#/c/4978/

Change-Id: I0e0d6762d1b2aeeb2109a358dffe62ecc825c932

@alviteri: Port by me to crDroid Android

This commit is special due to Lollipop is using Telephony repo for notify voicecalls so this commit is divided in two (2/2)

Conflicts:
	src/com/android/phone/NotificationMgr.java

src/com/android/server/telecom/MissedCallNotifier.java
@@ -34,6 +34,7 @@ import android.provider.Settings;
 import android.os.UserHandle;
 import android.provider.CallLog;
 import android.provider.CallLog.Calls;
+import android.provider.Settings;
 import android.telecom.CallState;
 import android.telecom.DisconnectCause;
 import android.telecom.PhoneAccount;
@@ -130,7 +131,9 @@ class MissedCallNotifier extends CallsManagerListenerBase {
 
         // Create the notification.
         Notification.Builder builder = new Notification.Builder(mContext);
-        builder.setSmallIcon(android.R.drawable.stat_notify_missed_call)
+        if (Settings.CMREMIX.getInt(mContext.getContentResolver(),
+               Settings.CMREMIX.KEY_MISSED_CALL_BREATH, 0) == 1) {
+             builder.setSmallIcon(R.drawable.stat_notify_missed_call_breath)
                 .setColor(mContext.getResources().getColor(R.color.theme_color))
                 .setWhen(call.getCreationTimeMillis())
                 .setContentTitle(mContext.getText(titleResId))
@@ -138,6 +141,16 @@ class MissedCallNotifier extends CallsManagerListenerBase {
                 .setContentIntent(createCallLogPendingIntent())
                 .setAutoCancel(true)
                 .setDeleteIntent(createClearMissedCallsPendingIntent());
+             } else {
+             builder.setSmallIcon(android.R.drawable.stat_notify_missed_call)
+                .setColor(mContext.getResources().getColor(R.color.theme_color))
+                .setWhen(call.getCreationTimeMillis())
+                .setContentTitle(mContext.getText(titleResId))
+                .setContentText(expandedText)
+                .setContentIntent(createCallLogPendingIntent())
+                .setAutoCancel(true)
+                .setDeleteIntent(createClearMissedCallsPendingIntent());
+             }
 
         Uri handleUri = call.getHandle();
         String handle = handleUri == null ? null : handleUri.getSchemeSpecificPart();

AICP/packages_services_Telecomm

src/com/android/server/telecom/MissedCallNotifier.java
@@ -146,12 +146,24 @@ class MissedCallNotifier extends CallsManagerListenerBase {
 
         // Create the notification.
         Notification.Builder builder = new Notification.Builder(mContext);
-        builder.setSmallIcon(android.R.drawable.stat_notify_missed_call)
+        if (Settings.System.getInt(mContext.getContentResolver(),
+               Settings.System.KEY_MISSED_CALL_BREATH, 0) == 1) {
+             builder.setSmallIcon(R.drawable.stat_notify_missed_call_breath)
                 .setColor(mContext.getResources().getColor(R.color.theme_color))
                 .setWhen(call.getCreationTimeMillis())
                 .setContentIntent(createCallLogPendingIntent())
                 .setAutoCancel(true)
                 .setDeleteIntent(createClearMissedCallsPendingIntent());
+             } else {
+             builder.setSmallIcon(android.R.drawable.stat_notify_missed_call)
+                .setColor(mContext.getResources().getColor(R.color.theme_color))
+                .setWhen(call.getCreationTimeMillis())
+                .setContentTitle(mContext.getText(titleResId))
+                .setContentText(expandedText)
+                .setContentIntent(createCallLogPendingIntent())
+                .setAutoCancel(true)
+                .setDeleteIntent(createClearMissedCallsPendingIntent());
+             }
 
         // display the first line of the notification:
         // 1 missed call: call name